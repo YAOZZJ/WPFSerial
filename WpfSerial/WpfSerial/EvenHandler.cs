dc067b92418781acd63e342755886d5049797c4c
YAOZZJ <yaozzj@163.com>
2019-06-18 10:57:25 +0800

    cmb列表增加,clear txt,COM菜单完善

--- WpfSerial/WpfSerial/EvenHandler.cs
+++ WpfSerial/WpfSerial/EvenHandler.cs
@@ -6,6 +6,39 @@ namespace WpfSerial
 {
     public partial class MainWindow : Window
     {
+        private void Startup()
+        {
+            foreach (string str1 in SerialClass.GetSerialPropertyValue(SerialClass.SerialProperty.PortName))
+            {
+                cmbSerialPortName.Items.Add(str1);
+            }
+            foreach (string str1 in SerialClass.GetSerialPropertyValue(SerialClass.SerialProperty.BaudRate))
+            {
+                cmbBaudRate.Items.Add(str1);
+            }
+            foreach (string str1 in SerialClass.GetSerialPropertyValue(SerialClass.SerialProperty.DataBits))
+            {
+                cmbDataBits.Items.Add(str1);
+            }
+            foreach (string str1 in SerialClass.GetSerialPropertyValue(SerialClass.SerialProperty.StopBits))
+            {
+                cmbStopbits.Items.Add(str1);
+            }
+            foreach (string str1 in SerialClass.GetSerialPropertyValue(SerialClass.SerialProperty.Parity))
+            {
+                cmbParity.Items.Add(str1);
+            }
+            //***************************************************************
+            cmbSerialPortName.SelectedIndex = 0;
+            cmbBaudRate.SelectedIndex = 1;
+            cmbDataBits.SelectedIndex = 0;
+            cmbStopbits.SelectedIndex = 1;
+            cmbParity.SelectedIndex = 0;
+            //***************************************************************
+            //btnSendData.IsEnabled = false;
+            //***************************************************************
+
+        }
         /// <summary>
         /// 退出
         /// </summary>
@@ -15,7 +48,12 @@ namespace WpfSerial
         {
             Close();
         }
-        private void MnuItmFleshClick(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// 刷新端口号
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MnuItmFreshClick(object sender, RoutedEventArgs e)
         {
             MnuItmSerial.Items.Clear();
             //int length = UsrCommunication.SerialClass.GetPortName().Length;
@@ -41,14 +79,21 @@ namespace WpfSerial
                 MnuItmSerial.Items.Add(mnuItm1);
             }
         }
+        /// <summary>
+        /// 通讯方式/端口选择
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MnuItmCommMethodClick(object sender, RoutedEventArgs e)
         {
             MenuItem mnuItm1 = (MenuItem)sender;
+            string header = (string)mnuItm1.Header;
+            MnuItmFreshClick(sender, e);//清除串口isChecked状态
             //StatItmMessage.Content = mnuItm1.Header;
-            switch (mnuItm1.Header)
+            switch (header)
             {
                 case "_TCPClient":
-                    if(MnuItmTCPClient.IsChecked)
+                    if (MnuItmTCPClient.IsChecked)
                     {
                         MnuItmTCPServer.IsChecked = false;
                         MnuItmUDP.IsChecked = false;
@@ -68,15 +113,51 @@ namespace WpfSerial
                         MnuItmTCPServer.IsChecked = false;
                     }
                     break;
-                case "COM1":
-                case "COM2":
-                case "COM3":
-                case "COM4":
-                    //StatItmMessage.Content = MnuItmSerial.Items.CurrentItem.ToString();
-                    break;
                 default:
+                    //串口选择,重新刷新端口,有没有其他不需要刷新的方法?
+                    if (header.Contains("COM"))
+                    {
+                        MnuItmTCPServer.IsChecked = false;
+                        MnuItmUDP.IsChecked = false;
+                        MnuItmTCPClient.IsChecked = false;
+                        MnuItmSerial.Items.Clear();
+                        foreach (string portName in UsrCommunication.SerialClass.GetPortName())
+                        {
+                            MenuItem mnuItm2 = new MenuItem();
+                            mnuItm2.Header = portName;
+                            mnuItm2.IsCheckable = true;
+                            if (portName == header)
+                            {
+                                mnuItm2.IsChecked = true;
+                            }
+                            else
+                            {
+                                mnuItm2.IsChecked = false;
+                            }
+                            mnuItm2.Click += MnuItmCommMethodClick;
+                            MnuItmSerial.Items.Add(mnuItm2);
+                        }
+                        //MnuItmFleshClick(sender, e);
+                        //StatItmMessage.Content = MnuItmSerial.Items.CurrentItem.ToString();
+                    }
                     break;
             }
+        }
+        private void BtnOpenSerialPortClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void BtnRefreshSerialPortClick(object sender, RoutedEventArgs e)
+        {
+            MnuItmFreshClick(sender, e);
+        }
+        private void BtnClearRecvDataClick(object sender, RoutedEventArgs e)
+        {
+            txtTerminal.Clear();
+        }
+        private void BtnClearSendDataClick(object sender, RoutedEventArgs e)
+        {
+            txtSendData.Clear();
         }
     }
 }
